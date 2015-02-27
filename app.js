a60f38034b67c7b9e6a4795cadcd36b6790f86b0
7yl4r <murray.tylar@gmail.com>
2015-02-27 16:55:39 -0500

    now supporting updating graphs and multiple issues

--- app.js
+++ app.js
@@ -1,41 +1,58 @@
 (function() {
     var app = angular.module('poddge', ["milestone-form-directives", 'nvd3ChartDirectives']);
 
-    app.controller('editorController', function(){
-        var editor = this;
-        this.getFakeMilestone = function(){
-            return {
-                name: 'resolve issue x',
-                hours: 10,
-                uncertainty: 4
-            };
+    app.getFakeMilestone = function(milestoneName){
+        if (typeof milestoneName === "undefined") {
+            var name = "resolve issue #" + (app.milestones.length+1);
+        } else {
+            var name = milestoneName;
+        }
+        return {
+            name: name,
+            hours: 10,
+            uncertainty: 4
         };
+    };
 
-        app.milestones = [editor.getFakeMilestone()];
+    app.milestones = [app.getFakeMilestone("resolve issue #1")];
+
+    app.getData = function() {
+        // TODO: don't recalc all of it, only what has changed
+        var data = [];
+        var cumulative = 0;
+        var nSamples = 5; // 1/2 number of samples per distribution curve
+        app.milestones.forEach(function (entry) {
+            data.push({key: entry.name, values: []});
+            for (var i = -nSamples; i <= nSamples; i++) {
+                var t = cumulative + entry.hours + entry.uncertainty * i / nSamples;
+                var sigma = entry.uncertainty / 2;
+                var mu = cumulative + entry.hours;
+                var p = Math.exp(-Math.pow(t - mu, 2) / (2 * Math.pow(sigma, 2)))
+                    / (sigma * Math.sqrt(2 * Math.PI));
+                data[data.length - 1]["values"].push([t, p]);
+            }
+            cumulative += entry.hours;
+        });
+        window.data = data;
+        return data;
+    };
+
+    app.controller('editorController', function(){
         this.milestones = app.milestones;
-
         this.addMilestone = function(milestone){
-            editor.milestones.push(editor.getFakeMilestone());
+            app.milestones.push(app.getFakeMilestone());
         };
     });
 
     app.controller('graphCtrl', function($scope){
-        var data = [];
-        var cumulative = 0;
-        var nSamples = 10; // 1/2 number of samples per distribution curve
-        app.milestones.forEach(function(entry){
-            data.push({key:entry.name, values:[]});
-            for(var i = -nSamples; i <= nSamples; i++){
-                var t = cumulative + entry.hours + entry.uncertainty * i/nSamples;
-                var sigma = entry.uncertainty/2;
-                var mu = cumulative + entry.hours;
-                var p = Math.exp( - Math.pow(t-mu, 2)/(2*Math.pow(sigma, 2))  )
-                        /(sigma*Math.sqrt(2*Math.PI));
-                data[data.length-1]["values"].push([t, p]);
-            }
-            cumulative += entry.hours;
-        });
+        $scope.exampleData = app.getData();
 
-        $scope.exampleData = data;
+        // TODO: refresh on command, not set interval
+        setInterval(function() {
+            $scope.$apply(function() {
+                $scope.exampleData = app.getData();
+            });
+        }, 1000);
+
     });
 })();
