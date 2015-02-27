032ba704e381c7e7e0092d1f166fd6a18d954507
7yl4r <murray.tylar@gmail.com>
2015-02-27 15:34:40 -0500

    added graph

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-    var app = angular.module('poddge', ["milestone-form-directives"]);
+    var app = angular.module('poddge', ["milestone-form-directives", 'nvd3ChartDirectives']);
 
     app.controller('editorController', function(){
         var editor = this;
@@ -11,10 +11,36 @@
             };
         };
 
-        this.milestones = [editor.getFakeMilestone()];
+        app.milestones = [editor.getFakeMilestone()];
+        this.milestones = app.milestones;
 
         this.addMilestone = function(milestone){
             editor.milestones.push(editor.getFakeMilestone());
         };
+    });
+
+    app.controller('graphCtrl', function($scope){
+        var data = [];
+        var cumulative = 0;
+        var nSamples = 10; // 1/2 number of samples per distribution curve
+        app.milestones.forEach(function(entry){
+            for(var i = -nSamples; i <= nSamples; i++){
+                var t = cumulative + entry.hours + entry.uncertainty * i/nSamples;
+                var sigma = entry.uncertainty/2;
+                var mu = cumulative + entry.hours;
+                var p = Math.exp( - Math.pow(t-mu, 2)/(2*Math.pow(sigma, 2))  )
+                        /(sigma*Math.sqrt(2*Math.PI));
+                data.push([t, p]);
+            }
+            cumulative += entry.hours;
+
+        });
+
+        $scope.exampleData = [
+            {
+                "key": "Series 1",
+                "values": data
+            }
+        ];
     });
 })();
